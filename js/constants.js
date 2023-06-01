132017c6ac91a136ee5842906506ae71687818cb
Ana Jin <anainesanxv@gmail.com>
2023-06-01 16:57:43 +0100

    first changes: directory images and file constants

---
+++ js/constants.js
@@ -0,0 +1,47 @@
+//OVNI's body
+const body_radius = 3; // usar scale para diminuir em y = 1/3*body
+
+//OVNI´s head
+const head_radius = 2;
+
+//ONVI's cilinder
+const cilinder_height = 0.2;
+const cilinder_radius = 1;
+
+//OVNI's lights 
+const light_radius = 0.5;
+
+//HOUSE 
+const house_lenght = 13;
+const house_height = 4;
+const house_width = 7;
+
+//HOUSE ceiling
+const ceiling_lenght = 13;
+const ceiling_height = 2;
+const ceiling_width = 7;
+
+//HOUSE chimney
+const chimney_length = 3;
+const chimney_height = 2.5;
+const chimney_width = 1;
+
+//HOUSE door
+const door_length = 1;
+const door_height = 2.5;
+
+//HOUSE windows
+const window_edge_length = 1;
+
+const default_width = 0.2;
+
+//TREE
+const main_log_radius = 0.75;
+const main_log_height = 4;
+
+//TREE side log
+const side_log_radius = 0.5;
+const side_log_height = 2.5;
+
+//TREE leaves
+const leaves_radius = 3.5; //scale y = 1/1.75
--- js/main-script.js
+++ js/main-script.js
@@ -61,6 +61,49 @@ function render() {
 ////////////////////////////////
 function init() {
     'use strict';
+    // Configuração básica
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Adicionando uma geometria para representar o terreno
+const groundGeometry = new THREE.PlaneGeometry(10, 10);
+const groundTexture = new THREE.TextureLoader().load('images/woodland.jpg');
+const groundMaterial = new THREE.MeshBasicMaterial({ map: groundTexture });
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2; // Rotação para posicionar o plano horizontalmente
+scene.add(ground);
+
+// Adicionando árvores dispersas no cenário
+const treeGeometry = new THREE.CylinderGeometry(0.1, 0.1, 1, 8);
+const treeTexture = new THREE.TextureLoader().load('images/trunk.jpg');
+const treeMaterial = new THREE.MeshBasicMaterial({ map: treeTexture });
+for (let i = 0; i < 20; i++) {
+  const tree = new THREE.Mesh(treeGeometry, treeMaterial);
+  tree.position.x = Math.random() * 10 - 5; // Posição x aleatória
+  tree.position.z = Math.random() * 10 - 5; // Posição z aleatória
+  tree.position.y = 0.5; // Altura do tronco da árvore
+  scene.add(tree);
+}
+
+// Adicionando uma casa alentejana à direita do cenário
+//const houseGeometry = new THREE.BoxGeometry(2, 1, 2);
+//const houseTexture = new THREE.TextureLoader().load('textures/house.jpg');
+//const houseMaterial = new THREE.MeshBasicMaterial({ map: houseTexture });
+//const house = new THREE.Mesh(houseGeometry, houseMaterial);
+//house.position.x = 3; // Posição x da casa
+//house.position.y = 0.5; // Altura da casa
+//scene.add(house);
+
+// Configuração da câmera
+camera.position.z = 5;
+camera.position.y = 1;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+animate();
+
 
 }
 
@@ -69,6 +112,8 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 
 }
 
