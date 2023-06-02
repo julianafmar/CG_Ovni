fdce7444be889015c6a6ac3e5b3181ff053233d2
julianafmar <juliana.marcelino@tecnico.ulisboa.pt>
2023-06-02 16:32:54 +0100

    Flowers done with land as a solid color

--- js/main-script.js
+++ js/main-script.js
@@ -1,7 +1,8 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-var renderer, scene;
+var renderer, scene, mesh, material;
+var fieldTexture, skyTexture, currentTexture;
 
 var cameras = [];
 var activeCamera = 0;
@@ -63,6 +64,75 @@ function createCamera(){
 /* CREATE OBJECT3D(S) */
 ////////////////////////
 
+function generateFieldTexture() {
+    var canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    var ctx = canvas.getContext('2d');
+
+    // Fundo verde claro
+    ctx.fillStyle = '#C1E685';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Círculos brancos, amarelos, lilases e azuis-claros
+    var colors = ['#FFFFFF', '#FFFF00', '#C8A2C8', '#ADD8E6'];
+    var numCircles = 50;
+    var radiusRange = [1, 5];
+    var margin = 10; // Margin to avoid flowers near the edges
+
+    for (var i = 0; i < numCircles; i++) {
+        var x = Math.random() * (canvas.width - margin * 2) + margin;
+        var y = Math.random() * (canvas.height - margin * 2) + margin;
+        var radius = Math.random() * (radiusRange[1] - radiusRange[0]) + radiusRange[0];
+        var color = colors[Math.floor(Math.random() * colors.length)];
+
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    fieldTexture = new THREE.Texture(canvas);
+    fieldTexture.needsUpdate = true;
+}
+
+
+function generateSkyTexture() {
+    var canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    var ctx = canvas.getContext('2d');
+
+    // Fundo degradê de azul-escuro para violeta-escuro
+    var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    gradient.addColorStop(0, '#00008B'); // Azul-escuro
+    gradient.addColorStop(1, '#8A2BE2'); // Violeta-escuro
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Pontilhado de estrelas brancas
+    var numStars = 300;
+    var radiusRange = [0.5, 2];
+
+    ctx.fillStyle = '#FFFFFF';
+
+    for (var i = 0; i < numStars; i++) {
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        var radius = Math.random() * (radiusRange[1] - radiusRange[0]) + radiusRange[0];
+
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    skyTexture = new THREE.Texture(canvas);
+    skyTexture.needsUpdate = true;
+}
+
 //////////////////////
 /* CHECK COLLISIONS */
 //////////////////////
@@ -113,17 +183,20 @@ function init() {
 
     createScene();
     createCamera();
+    generateFieldTexture();
+    currentTexture = fieldTexture;
+
+    var geometry = new THREE.PlaneGeometry(10, 10);
+    material = new THREE.MeshBasicMaterial({ map: currentTexture });
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("resize", onResize);
     window.addEventListener("keyup", onKeyUp);
 
-    // Configuração básica
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
     // Adicionando uma geometria para representar o terreno
-    const groundGeometry = new THREE.PlaneGeometry(10, 10);
+    /*const groundGeometry = new THREE.PlaneGeometry(10, 10);
     const groundTexture = new THREE.TextureLoader().load('images/woodland.jpg');
     const groundMaterial = new THREE.MeshBasicMaterial({ map: groundTexture });
     const ground = new THREE.Mesh(groundGeometry, groundMaterial);
@@ -140,24 +213,9 @@ function init() {
         tree.position.z = Math.random() * 10 - 5; // Posição z aleatória
         tree.position.y = 0.5; // Altura do tronco da árvore
         scene.add(tree);
-    }
+    }*/
 
-// Adicionando uma casa alentejana à direita do cenário
-//const houseGeometry = new THREE.BoxGeometry(2, 1, 2);
-//const houseTexture = new THREE.TextureLoader().load('textures/house.jpg');
-//const houseMaterial = new THREE.MeshBasicMaterial({ map: houseTexture });
-//const house = new THREE.Mesh(houseGeometry, houseMaterial);
-//house.position.x = 3; // Posição x da casa
-//house.position.y = 0.5; // Altura da casa
-//scene.add(house);
-
-// Configuração da câmera
-camera.position.z = 5;
-camera.position.y = 1;
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-animate();
-
+    animate();
 
 }
 
@@ -193,6 +251,17 @@ function onResize() {
 ///////////////////////
 function onKeyDown(e) {
     'use strict';
+
+    switch (e.keyCode) {
+        case 49: // Tecla 1
+            currentTexture = fieldTexture;
+            mesh.material.map = currentTexture;
+            break;
+        case 50: // Tecla 2
+            currentTexture = skyTexture;
+            mesh.material.map = currentTexture;
+            break;
+    }
 
 }
 
